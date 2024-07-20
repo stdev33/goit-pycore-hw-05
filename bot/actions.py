c912bb5d5f9567ab332c48d40a2171127a449604
Sergej Tarasenko <sergej.tarasenko@murka.com>
2024-07-20 13:08:09 +0300

    task 4. add input errors handling for cli bot using input_error decorator

--- bot/actions.py
+++ bot/actions.py
@@ -1,29 +1,35 @@
+from .helpers import input_error
+
+@input_error
 def add_contact(args, contacts):
     if len(args) != 2:
-        return "Invalid input. Usage: add [name] [phone]"
+        raise ValueError
     name, phone = args
     contacts[name] = phone
     return "Contact added."
 
+@input_error
 def change_contact(args, contacts):
     if len(args) != 2:
-        return "Invalid input. Usage: change [name] [phone]"
+        raise ValueError
     name, phone = args
     if name in contacts:
         contacts[name] = phone
         return "Contact updated."
     else:
-        return "Contact not found."
+        raise KeyError
 
+@input_error
 def show_phone(args, contacts):
     if len(args) != 1:
-        return "Invalid input. Usage: phone [name]"
+        raise IndexError
     name = args[0]
     if name in contacts:
         return contacts[name]
     else:
-        return "Contact not found."
+        raise KeyError
 
+@input_error
 def show_all(contacts):
     if contacts:
         return "\n".join(f"{name}: {phone}" for name, phone in contacts.items())
